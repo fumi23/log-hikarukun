54b339ede63b180c96c31c0c1888fb1c7659694a
fumi <fumihiro.takahashi@studist.jp>
2023-11-04 03:44:25 +0900

    MutationObserver

--- contents.js
+++ contents.js
@@ -10,12 +10,27 @@ function *generator() {
   }
 }
 
-// element: タイムスタンプが記載されている表のセル要素
-// delta: 直前行のタイムスタンプからの増分（単位は ms, 最初はNaN）
-for (const [element, delta] of generator()) {
-  // deltaの値 [0, 1023] を対数スケールで [0, 1] に変換する
-  const scaled = Math.log1p(delta) / Math.log(1024)
-  // セル要素の背景色とアドバイザリー情報を設定する
-  element.style.backgroundColor = `hsla(50, 100%, 50%, ${scaled})`
-  element.title = `${delta}ms (scale: ${scaled.toFixed(2)})`
+function colorizeTable() {
+  // element: タイムスタンプが記載されている表のセル要素
+  // delta: 直前行のタイムスタンプからの増分（単位は ms, 最初はNaN）
+  for (const [element, delta] of generator()) {
+    // deltaの値 [0, 1023] を対数スケールで [0, 1] に変換する
+    const scaled = Math.log1p(delta) / Math.log(1024)
+    // セル要素の背景色とアドバイザリー情報を設定する
+    element.style.backgroundColor = `hsla(50, 100%, 50%, ${scaled})`
+    element.title = `${delta}ms (scale: ${scaled.toFixed(2)})`
+  }
 }
+
+function main() {
+  // DOM構築済みの表に色を付ける
+  colorizeTable()
+  // 表のDOM更新を監視し、変更を検知する都度再実行する
+  const observer = new MutationObserver(() => { colorizeTable() })
+  const target = document.querySelector('tbody')
+  if (target) {
+    observer.observe(target, { subtree: true, childList: true })
+  }
+}
+
+main() // 拡張機能アイコンをクリックすると実行される
