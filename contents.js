9cd9f3b70c3160dc584ef49d6fde162756ab406a
fumi <fumihiro.takahashi@studist.jp>
2023-11-04 02:30:34 +0900

    Generatorでロジックを分離

--- contents.js
+++ contents.js
@@ -1,27 +1,21 @@
-function queryDataCellsInColumn(column) {
-  const selector = `tbody tr td:nth-of-type(${column})`
-  return document.querySelectorAll(selector)
-}
-
-function generateCallback({ timestamp, modifier }) {
+function *generator() {
+  // 表の行番号(#)を除く最初の列をタイムスタンプ列とみなす
+  const timestampCells = document.querySelectorAll('tbody tr td:nth-of-type(2)')
   let prevTimestamp = NaN
-  return function(node) {
-    const currentTimestamp = timestamp(node)
-    const elapsed = currentTimestamp - prevTimestamp
-    modifier(node, elapsed)
+  for (const timestampCell of timestampCells.values()) {
+    // 表のセル内の文字列が ISO 8601 形式であることを期待している
+    const currentTimestamp = Date.parse(timestampCell.textContent)
+    yield [timestampCell, currentTimestamp - prevTimestamp]
     prevTimestamp = currentTimestamp
   }
 }
 
-const nodeList = queryDataCellsInColumn(2)
-const callback = generateCallback({
-  timestamp(node) {
-    return Date.parse(node.textContent)
-  },
-  modifier(node, elapsed) {
-    const alpha = Math.log1p(elapsed) / Math.log(1024)
-    node.style.backgroundColor = `hsla(50, 100%, 50%, ${alpha})`
-    node.title = `${elapsed}ms`
-  }
-})
-nodeList.forEach(callback)
+// element: タイムスタンプが記載されている表のセル要素
+// delta: 直前行のタイムスタンプからの増分（単位は ms, 最初はNaN）
+for (const [element, delta] of generator()) {
+  // deltaの値 [0, 1023] を対数スケールで [0, 1] に変換する
+  const scaled = Math.log1p(delta) / Math.log(1024)
+  // セル要素の背景色とアドバイザリー情報を設定する
+  element.style.backgroundColor = `hsla(50, 100%, 50%, ${scaled})`
+  element.title = `${delta}ms (scale: ${scaled.toFixed(2)})`
+}
